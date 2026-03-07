3feba5d8b2d5abdb12007b9b4b5ec45c6d0b7e02
mk-404lab <alsgur9984@naver.com>
2026-03-08 01:48:53 +0900

    feat: 2-11 URL 프리픽스 사용

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,23 +5,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+/*
+@RequestMapping("/question")
+=> 이는 서버에서 url을 작성할 때 question을 생략하기 위해 사용하는 어노테이션이 아님
+=> 컨트롤러에서 매핑할 때 question이 중복되니까 그걸 피하고자 사용하는 것
+=> url은 /question/list 원래대로 사용해야 되고, 컨트롤러가 자체적으로 /question + /list로 매핑해줌
+ */
+
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
